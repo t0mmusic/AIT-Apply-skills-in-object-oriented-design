220b1e2faf28d8fd5425141617835565a68383e7
Jordan Brown <jebrown2091@gmail.com>
2023-01-25 16:38:13 +1030

    fun with themes

--- gui.py
+++ gui.py
@@ -1,8 +1,14 @@
 import login
 import fish
+import sys
+import random
 import PySimpleGUI as sg
+import os.path
+from os import path
 
-sg.theme('DarkRed')
+themes = ['Black', 'BlueMono', 'BluePurple', 'BrightColors', 'BrownBlue', 'Dark', 'Dark2', 'DarkAmber', 'DarkBlack', 'DarkBlack1', 'DarkBlue', 'DarkBlue1', 'DarkBlue10', 'DarkBlue11', 'DarkBlue12', 'DarkBlue13', 'DarkBlue14', 'DarkBlue15', 'DarkBlue16', 'DarkBlue17', 'DarkBlue2', 'DarkBlue3', 'DarkBlue4', 'DarkBlue5', 'DarkBlue6', 'DarkBlue7', 'DarkBlue8', 'DarkBlue9', 'DarkBrown', 'DarkBrown1', 'DarkBrown2', 'DarkBrown3', 'DarkBrown4', 'DarkBrown5', 'DarkBrown6', 'DarkGreen', 'DarkGreen1', 'DarkGreen2', 'DarkGreen3', 'DarkGreen4', 'DarkGreen5', 'DarkGreen6', 'DarkGrey', 'DarkGrey1', 'DarkGrey2', 'DarkGrey3', 'DarkGrey4', 'DarkGrey5', 'DarkGrey6', 'DarkGrey7', 'DarkPurple', 'DarkPurple1', 'DarkPurple2', 'DarkPurple3', 'DarkPurple4', 'DarkPurple5', 'DarkPurple6', 'DarkRed', 'DarkRed1', 'DarkRed2', 'DarkTanBlue', 'DarkTeal', 'DarkTeal1', 'DarkTeal10', 'DarkTeal11', 'DarkTeal12', 'DarkTeal2', 'DarkTeal3', 'DarkTeal4', 'DarkTeal5', 'DarkTeal6', 'DarkTeal7', 'DarkTeal8', 'DarkTeal9', 'Default', 'Default1', 'DefaultNoMoreNagging', 'Green', 'GreenMono', 'GreenTan', 'HotDogStand', 'Kayak', 'LightBlue', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4', 'LightBlue5', 'LightBlue6', 'LightBlue7', 'LightBrown', 'LightBrown1', 'LightBrown10', 'LightBrown11', 'LightBrown12', 'LightBrown13', 'LightBrown2', 'LightBrown3', 'LightBrown4', 'LightBrown5', 'LightBrown6', 'LightBrown7', 'LightBrown8', 'LightBrown9', 'LightGray1', 'LightGreen', 'LightGreen1', 'LightGreen10', 'LightGreen2', 'LightGreen3', 'LightGreen4', 'LightGreen5', 'LightGreen6', 'LightGreen7', 'LightGreen8', 'LightGreen9', 'LightGrey', 'LightGrey1', 'LightGrey2', 'LightGrey3', 'LightGrey4', 'LightGrey5', 'LightGrey6', 'LightPurple', 'LightTeal', 'LightYellow', 'Material1', 'Material2', 'NeutralBlue', 'Purple', 'Reddit', 'Reds', 'SandyBeach', 'SystemDefault', 'SystemDefault1', 'SystemDefaultForReal', 'Tan', 'TanBlue', 'TealMono', 'Topanga']
+
+sg.theme(themes[random.randint(0, len(themes))])
 # layout for the login selection screen
 layoutLog = [ 
 			[sg.Text("Username", size=(15, 1)), sg.Input(key='USER')],
@@ -103,7 +109,7 @@ def preGameMenu():
 		# Exits cleanly if user closes window
 		if (event == sg.WINDOW_CLOSED or event == 'EXIT'):
 			window.close()
-			exit()
+			break
 		# Displays LeaderBoard
 		if (event == 'SCORE'):
 			window['INFO'].update("")
@@ -143,10 +149,14 @@ def preGameMenu():
 			if (ret != False):
 				currentColumn.update(visible=False)
 				return (ret)
+	window.close()
+	sys.exit()
 
 # updates the image to the appropriate matching fish. If there
 # are more than 6 fish, others are given a placeholder image.
 def updateImage(fish):
+	if (path.exists('KGW.png') == False):
+		return
 	if (fish == "King George Whiting"):
 		img = 'KGW.png'
 	elif (fish == "Lost Bait"):
