a3d95e56cb1efdbab72ef39178f142ddb1feac4e
Jordan Brown <jebrown2091@gmail.com>
2023-01-25 21:53:15 +1030

    updated bucket

--- gui.py
+++ gui.py
@@ -6,11 +6,39 @@ import PySimpleGUI as sg
 import os.path
 from os import path
 
-themes = ['Black', 'BlueMono', 'BluePurple', 'BrightColors', 'BrownBlue', 'Dark', 'Dark2', 'DarkAmber', 'DarkBlack', 'DarkBlack1', 'DarkBlue', 'DarkBlue1', 'DarkBlue10', 'DarkBlue11', 'DarkBlue12', 'DarkBlue13', 'DarkBlue14', 'DarkBlue15', 'DarkBlue16', 'DarkBlue17', 'DarkBlue2', 'DarkBlue3', 'DarkBlue4', 'DarkBlue5', 'DarkBlue6', 'DarkBlue7', 'DarkBlue8', 'DarkBlue9', 'DarkBrown', 'DarkBrown1', 'DarkBrown2', 'DarkBrown3', 'DarkBrown4', 'DarkBrown5', 'DarkBrown6', 'DarkGreen', 'DarkGreen1', 'DarkGreen2', 'DarkGreen3', 'DarkGreen4', 'DarkGreen5', 'DarkGreen6', 'DarkGrey', 'DarkGrey1', 'DarkGrey2', 'DarkGrey3', 'DarkGrey4', 'DarkGrey5', 'DarkGrey6', 'DarkGrey7', 'DarkPurple', 'DarkPurple1', 'DarkPurple2', 'DarkPurple3', 'DarkPurple4', 'DarkPurple5', 'DarkPurple6', 'DarkRed', 'DarkRed1', 'DarkRed2', 'DarkTanBlue', 'DarkTeal', 'DarkTeal1', 'DarkTeal10', 'DarkTeal11', 'DarkTeal12', 'DarkTeal2', 'DarkTeal3', 'DarkTeal4', 'DarkTeal5', 'DarkTeal6', 'DarkTeal7', 'DarkTeal8', 'DarkTeal9', 'Default', 'Default1', 'DefaultNoMoreNagging', 'Green', 'GreenMono', 'GreenTan', 'HotDogStand', 'Kayak', 'LightBlue', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4', 'LightBlue5', 'LightBlue6', 'LightBlue7', 'LightBrown', 'LightBrown1', 'LightBrown10', 'LightBrown11', 'LightBrown12', 'LightBrown13', 'LightBrown2', 'LightBrown3', 'LightBrown4', 'LightBrown5', 'LightBrown6', 'LightBrown7', 'LightBrown8', 'LightBrown9', 'LightGray1', 'LightGreen', 'LightGreen1', 'LightGreen10', 'LightGreen2', 'LightGreen3', 'LightGreen4', 'LightGreen5', 'LightGreen6', 'LightGreen7', 'LightGreen8', 'LightGreen9', 'LightGrey', 'LightGrey1', 'LightGrey2', 'LightGrey3', 'LightGrey4', 'LightGrey5', 'LightGrey6', 'LightPurple', 'LightTeal', 'LightYellow', 'Material1', 'Material2', 'NeutralBlue', 'Purple', 'Reddit', 'Reds', 'SandyBeach', 'SystemDefault', 'SystemDefault1', 'SystemDefaultForReal', 'Tan', 'TanBlue', 'TealMono', 'Topanga']
+# array of all PySimpleGui Themes for randomisation
+themes = ['Black', 'BlueMono', 'BluePurple', 'BrightColors', 'BrownBlue',
+			'Dark', 'Dark2', 'DarkAmber', 'DarkBlack', 'DarkBlack1', 'DarkBlue',
+			'DarkBlue1', 'DarkBlue10', 'DarkBlue11', 'DarkBlue12', 'DarkBlue13',
+			'DarkBlue14', 'DarkBlue15', 'DarkBlue16', 'DarkBlue17', 'DarkBlue2',
+			'DarkBlue3', 'DarkBlue4', 'DarkBlue5', 'DarkBlue6', 'DarkBlue7',
+			'DarkBlue8', 'DarkBlue9', 'DarkBrown', 'DarkBrown1', 'DarkBrown2',
+			'DarkBrown3', 'DarkBrown4', 'DarkBrown5', 'DarkBrown6', 'DarkGreen',
+			'DarkGreen1', 'DarkGreen2', 'DarkGreen3', 'DarkGreen4', 'DarkGreen5',
+			'DarkGreen6', 'DarkGrey', 'DarkGrey1', 'DarkGrey2', 'DarkGrey3',
+			'DarkGrey4', 'DarkGrey5', 'DarkGrey6', 'DarkGrey7', 'DarkPurple',
+			'DarkPurple1', 'DarkPurple2', 'DarkPurple3', 'DarkPurple4', 'DarkPurple5',
+			'DarkPurple6', 'DarkRed', 'DarkRed1', 'DarkRed2', 'DarkTanBlue', 'DarkTeal',
+			'DarkTeal1', 'DarkTeal10', 'DarkTeal11', 'DarkTeal12', 'DarkTeal2', 'DarkTeal3',
+			'DarkTeal4', 'DarkTeal5', 'DarkTeal6', 'DarkTeal7', 'DarkTeal8', 'DarkTeal9',
+			'Default', 'Default1', 'DefaultNoMoreNagging', 'Green', 'GreenMono', 'GreenTan',
+			'HotDogStand', 'Kayak', 'LightBlue', 'LightBlue1', 'LightBlue2', 'LightBlue3',
+			'LightBlue4', 'LightBlue5', 'LightBlue6', 'LightBlue7', 'LightBrown',
+			'LightBrown1', 'LightBrown10', 'LightBrown11', 'LightBrown12', 'LightBrown13',
+			'LightBrown2', 'LightBrown3', 'LightBrown4', 'LightBrown5', 'LightBrown6',
+			'LightBrown7', 'LightBrown8', 'LightBrown9', 'LightGray1', 'LightGreen',
+			'LightGreen1', 'LightGreen10', 'LightGreen2', 'LightGreen3', 'LightGreen4',
+			'LightGreen5', 'LightGreen6', 'LightGreen7', 'LightGreen8', 'LightGreen9',
+			'LightGrey', 'LightGrey1', 'LightGrey2', 'LightGrey3', 'LightGrey4',
+			'LightGrey5', 'LightGrey6', 'LightPurple', 'LightTeal', 'LightYellow',
+			'Material1', 'Material2', 'NeutralBlue', 'Purple', 'Reddit', 'Reds',
+			'SandyBeach', 'SystemDefault', 'SystemDefault1', 'SystemDefaultForReal',
+			'Tan', 'TanBlue', 'TealMono', 'Topanga']
 
+# Sets a random theme from the array
 sg.theme(themes[random.randint(0, len(themes))])
 # layout for the login selection screen
-layoutLog = [ 
+layoutLog = [
 			[sg.Text("Username", size=(15, 1)), sg.Input(key='USER')],
 			[sg.Text("Password", size=(15, 1)), sg.Input(key='PASS', password_char='*')]
 		]
@@ -27,9 +55,9 @@ layoutGame = [
 			[sg.Button('Cast a line!', key='THROW'),
 			sg.Button('Keep', visible=False, key='KEEP'),
 			sg.Button('Release', visible=False, key='RELEASE')],
-			[sg.Multiline(size=(30, 5), visible=False, key='CATCHLIST')],
 			[sg.Text("Current Score: 0", key='CURR_SCORE')]
 		]
+layoutBucket = []
 # universal layout to display all features of the gui
 layout = [
 			[sg.Button('Log In', key='LOGIN_MENU'),
@@ -41,7 +69,8 @@ layout = [
 			[sg.Column(layoutLog, key='COL1'),
 			sg.Column(layoutSign, visible=False, key='COL2'),
 			sg.Column(layoutScore, visible=False, key='COL3'),
-			sg.Column(layoutGame, visible=False, key='COL4')],
+			sg.Column(layoutGame, visible=False, key='COL4'),
+			sg.Multiline(size=(30, 5), visible=False, key='CATCHLIST')],
 			[sg.Button('Start Playing!', key='LOGIN'),
 			sg.Button('Start Playing!', key='SIGNUP', visible=False)],
 			[sg.Image(key='IMG', visible=False)],
@@ -172,7 +201,7 @@ def updateImage(fish):
 	else:
 		img = 'fishing.png'
 	window['IMG'].update(img, size=(500, 300), visible=True)
-	
+
 
 # Gameplay menu for the gui. User can cast a line, then choose to keep
 # or release what they have caught.
@@ -190,9 +219,9 @@ def gamePlay(currUser):
 	window['LOGNAME'].update(currUser.username)
 	while (True):
 		event, values = window.read()
+		window.bind("<Return>", 'THROW')
 		if (event == sg.WINDOW_CLOSED or event == 'EXIT'):
 			break
-		window.bind("<Return>", 'THROW')
 		# User casts a line to catch a fish, caught fish is displayed
 		if (event == 'THROW'):
 			window.bind("<Return>", 'KEEP')
@@ -232,10 +261,18 @@ def gamePlay(currUser):
 		# displays the bucket of fish caught by the user
 		if (event == 'BUCKET'):
 			window['CATCHLIST'].update(visible=True)
+			window['LOGIN_MENU'].update("Back to game", visible=True)
+			window['LOGNAME'].update(visible=False)
+			window['BUCKET'].update(visible=False)
+			window['SCORE'].update(visible=False)
+			window['INFO'].update("")
+			currentColumn.update(visible=False)
 		# Displays LeaderBoard
 		if (event == 'SCORE'):
 			window['SCORE'].update(visible=False)
+			window['BUCKET'].update(visible=False)
 			window['CATCHLIST'].update(visible=False)
+			window['LOGNAME'].update(visible=False)
 			window['LOGIN_MENU'].update("Back to game", visible=True)
 			window['INFO'].update("")
 			currentColumn.update(visible=False)
@@ -246,9 +283,16 @@ def gamePlay(currUser):
 		if (event == 'LOGIN_MENU'):
 			window['INFO'].update("")
 			window['SCORE'].update(visible=True)
+			window['LOGNAME'].update(visible=True)
+			window['BUCKET'].update(visible=True)
 			window['LOGIN_MENU'].update(visible=False)
+			window['CATCHLIST'].update(visible=False)
 			currentColumn.update(visible=False)
 			window['COL4'].update(visible=True)
+			window['CURR_SCORE'].update(visible=True)
+			window['INFO'].update(visible=True)
+			window['KEEP'].update(visible=True)
+			window['RELEASE'].update(visible=True)
 			currentColumn = window['COL4']
 		# database is updated with new scores
 		login.Account.updateData()
